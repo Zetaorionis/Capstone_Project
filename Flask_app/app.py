c3b45ffe2dea3f309ebc5b5982a69772002b9404
Kevin Nazario <kevin.l.nazario@gmail.com>
2024-05-09 01:58:42 -0700

    Add code for potential interactive predictor

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, send_from_directory
+from flask import Flask, redirect, url_for, render_template, send_from_directory, jsonify, request
 
 app = Flask(__name__, static_url_path='',
             static_folder='static',
@@ -27,6 +27,18 @@ def endpoint2():
 @app.route('/endpoint3')
 def endpoint3():
     return redirect('https://github.com/Zetaorionis/Capstone_Project/tree/main')
+
+# API endpoint to Trained Model Landing Page
+@app.route('/result', methods = ['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        if int(to_predict_list['number']) > 5: 
+            prediction = 'Greater than 5'
+        else: 
+            prediction = 'Less than or equal to 5'
+    return render_template('result.html', prediction = prediction)
+
 
 # Serve static files like CSS, JS, and images
 # @app.route('/static/<path:filename>')
