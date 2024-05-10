cd8d6f13b47438e0b6284309dd453bf30b87a489
Kevin Nazario <kevin.l.nazario@gmail.com>
2024-05-09 19:09:00 -0700

    Updates to Flask App - ML Model

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,4 +1,8 @@
+# Import Dependencies
 from flask import Flask, redirect, url_for, render_template, send_from_directory, jsonify, request
+import pandas as pd
+import numpy as np
+import tensorflow as tf
 
 app = Flask(__name__, static_url_path='',
             static_folder='static',
@@ -6,6 +10,12 @@ app = Flask(__name__, static_url_path='',
 # app.config['TEMPLATES_AUTO_RELOAD'] = True  # Optional: Auto-reload templates during development
  
 dashboard = 'https://public.tableau.com/app/profile/viktor.kabelkov/viz/WineAnalysis_17149494454560/WineInfoDaashboard'
+
+# prediction function
+def ValuePredictor(predict_row):
+    loaded_model = tf.keras.models.load_model('static/resources/Wine2.h5')
+    result = loaded_model.predict(predict_row)
+    return result[0][0] 
 
 
 # Root endpoint with links to api1, api2, and api3
@@ -39,6 +49,35 @@ def result():
             prediction = 'Less than or equal to 5'
     return render_template('result.html', prediction = prediction)
 
+# API endpoint to Trained Model Landing Page
+@app.route('/result2', methods = ['POST'])
+def result2():
+    wine_df = pd.read_csv('static/resources/clean_wine_data_final.csv')
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        wine = int(to_predict_list['number'])
+        wine_row = wine_df.iloc[wine,:]
+        prediction = wine_row['title']
+
+    return render_template('result.html', prediction = prediction)
+
+# API endpoint to Trained Model Landing Page
+@app.route('/result3', methods = ['POST'])
+def result3():
+    wine_df = pd.read_csv('static/resources/clean_wine_data_final.csv')
+    scaled_df = pd.read_csv('static/resources/scaled_data_df.csv')
+
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        wine = int(to_predict_list['number'])
+        predict_row = np.expand_dims(scaled_df.iloc[wine,0:68], axis=0)
+        result = ValuePredictor(predict_row)   
+        if result < .5:
+            prediction = f"We predict this wine is less than 90 points (Probability: {round(float(result),2)})"
+        else: 
+            prediction = f"This wine is 90 points or more! (Probability: {round(float(result),2)})"
+
+    return render_template('result.html', prediction = prediction)
 
 # Serve static files like CSS, JS, and images
 # @app.route('/static/<path:filename>')
